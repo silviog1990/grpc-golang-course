ae1c67045f21cede5586ee933d940a860cd82e1b
Silvio Gay <gaysilvio90@gmail.com>
2020-04-19 16:06:22 +0200

    added unary api with deadline

--- unary/greeting/client/client.go
+++ unary/greeting/client/client.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/silviog1990/grpc-golang-course/unary/greeting/greetpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
@@ -18,6 +21,8 @@ func main() {
 	defer cc.Close()
 	c := greetpb.NewGreetServiceClient(cc)
 	doUnary(c)
+	doUnaryWithDeadline(c, 5*time.Second) // will succeed
+	doUnaryWithDeadline(c, 1*time.Second) // will fail
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -35,4 +40,34 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("error while calling Greet RPC: %v", err)
 	}
 	fmt.Printf("Response from Greet: %v\n", res.GetResult())
+}
+
+func doUnaryWithDeadline(c greetpb.GreetServiceClient, timeout time.Duration) {
+	fmt.Println("Start doUnaryWithDeadline invocation")
+
+	req := &greetpb.GreetWithDeadlineRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Silvio",
+			LastName:  "Gay",
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	res, err := c.GreetWithDeadline(ctx, req)
+	if err != nil {
+		statusErr, ok := status.FromError(err)
+		if ok {
+			if statusErr.Code() == codes.DeadlineExceeded {
+				fmt.Println("Timeout was hit! Deadline was exceeded")
+			} else {
+				fmt.Printf("unexpeted error: %v", statusErr)
+			}
+		} else {
+			log.Fatalf("error while calling Greet RPC: %v", err)
+		}
+		return
+	}
+	fmt.Printf("Response from GreetWithDeadline: %v\n", res.GetResult())
 }
--- unary/greeting/server/server.go
+++ unary/greeting/server/server.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/silviog1990/grpc-golang-course/unary/greeting/greetpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type server struct{}
@@ -18,6 +21,22 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 	firstname := req.GetGreeting().GetFirstName()
 	result := "Hello " + firstname
 	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
+func (*server) GreetWithDeadline(ctx context.Context, req *greetpb.GreetWithDeadlineRequest) (*greetpb.GreetWithDeadlineResponse, error) {
+	fmt.Printf("GreetWithDeadline function was invoked with: %v\n", req)
+	time.Sleep(3 * time.Second)
+	if ctx.Err() == context.Canceled {
+		errorMsg := "the client has canceled the request"
+		fmt.Println(errorMsg)
+		return nil, status.Error(codes.Canceled, errorMsg)
+	}
+	firstname := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstname
+	res := &greetpb.GreetWithDeadlineResponse{
 		Result: result,
 	}
 	return res, nil
